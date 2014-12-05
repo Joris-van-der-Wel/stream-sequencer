72639c648decf8bbda4c2a262a5fba85321d61b9
Joris van der Wel <joris@jorisvanderwel.com>
2014-12-05 12:22:53 +0100

    Support for integer wrapping in Rearranger

--- test/Rearranger.js
+++ test/Rearranger.js
@@ -85,5 +85,29 @@ module.exports = {
                 stream.write(new Buffer('00000001'+'0a0b0c0d0e0f10111213', 'hex')); // 1
                 stream.write(new Buffer('00000000'+'00010203040506070809', 'hex')); // 0
                 stream.end();
-        }
+        },
+        'wrapping': function(test)
+        {
+                var stream = new Rearranger({sequenceStart: 0xfffffffe});
+
+                var concatStream = concat(function(result)
+                {
+                        test.strictEqual(result.toString('hex'), '000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f');
+                        test.strictEqual(stream.lastSequenceID, 1);
+                        test.done();
+                });
+
+                stream.on('error', function(err)
+                {
+                        test.ok(false, 'error ' + err);
+                });
+
+                stream.pipe(concatStream);
+
+                stream.write(new Buffer('00000000'+'1415161718191a1b1c1d', 'hex')); // 2
+                stream.write(new Buffer('00000001'+'1e1f', 'hex')); // 3
+                stream.write(new Buffer('fffffffe'+'00010203040506070809', 'hex')); // 0
+                stream.write(new Buffer('ffffffff'+'0a0b0c0d0e0f10111213', 'hex')); // 1
+                stream.end();
+        },
 };
